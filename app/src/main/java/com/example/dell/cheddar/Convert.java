2a1ffd62c0d0a5d28149c27da7e0b49c41affa14
JuliusNM <juliusngigim@gmail.com>
2017-06-29 22:56:02 -1200

    Added add recipient page

---
+++ app/src/main/java/com/example/dell/cheddar/Convert.java
@@ -0,0 +1,13 @@
+package com.example.dell.cheddar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Convert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convert);
+    }
+}
