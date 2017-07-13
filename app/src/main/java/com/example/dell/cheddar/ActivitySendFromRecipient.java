905ee2dccc66ed121868c405acc6572787c4d019
JuliusNM <juliusngigim@gmail.com>
2017-07-12 12:43:59 -1200

    Bug fixes

--- app/src/main/java/com/example/dell/cheddar/ActivitySendFromRecipient.java
+++ app/src/main/java/com/example/dell/cheddar/ActivitySendFromRecipient.java
@@ -7,15 +7,22 @@ import android.view.View;
 import android.widget.Button;
 
 public class ActivitySendFromRecipient extends AppCompatActivity {
+    Button next;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_from_recipient);
 
-
-
-
+        next = (Button)findViewById(R.id.next_button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent convert = new Intent(getApplicationContext(), ConvertActivity.class);
+                startActivity(convert);
+            }
+        });
     }
 }
